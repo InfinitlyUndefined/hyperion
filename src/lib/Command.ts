f9bb8be361c9b578bfb2eb0bd35a092545b8d0ae
InfinitlyUndefined <absrdreality@gmail.com>
2024-08-24 21:17:08 -0500

    init

---
+++ src/lib/Command.ts
@@ -0,0 +1,58 @@
+import { ApplicationCommandType, AutocompleteInteraction, ChatInputCommandInteraction, MessageContextMenuCommandInteraction, UserContextMenuCommandInteraction, type ApplicationCommandOptionData, type PermissionResolvable } from "discord.js";
+
+type RunType = {
+    [ApplicationCommandType.ChatInput]: ChatInputCommandInteraction,
+    [ApplicationCommandType.User]: UserContextMenuCommandInteraction,
+    [ApplicationCommandType.Message]: MessageContextMenuCommandInteraction
+}
+
+interface BaseCommandOptions<T extends ApplicationCommandType> {
+    name: string
+    description?: string
+    type: T
+    defaultMemberPermissions?: PermissionResolvable[] | null
+    dmPermission?: boolean
+    guildIds?: string[]
+
+    callback: (interaction: RunType[T]) => Promise<void>
+}
+
+interface ChatInputCommandOptions extends BaseCommandOptions<ApplicationCommandType.ChatInput> {
+    description: string
+    type: ApplicationCommandType.ChatInput
+    options: ApplicationCommandOptionData[]
+
+    autoComplete?: (interaction: AutocompleteInteraction) => Promise<void>
+}
+
+type CommandOptions<T extends ApplicationCommandType> = T extends ApplicationCommandType.ChatInput ? ChatInputCommandOptions : BaseCommandOptions<T>
+
+export class Command<T extends ApplicationCommandType = ApplicationCommandType> {
+    public name: string;
+    public description: string
+    public type: ApplicationCommandType
+    public defaultMemberPermissions: PermissionResolvable[] | null
+    public dmPermission?: boolean;
+    public options?: ApplicationCommandOptionData[]
+    public guildIds?: string[]
+
+    public callback: (interaction: RunType[T]) => Promise<void>
+    public autoComplete?: (interaction: AutocompleteInteraction) => Promise<void>
+
+    public constructor(opts: CommandOptions<T>) {
+        this.name = opts.name
+        this.description = opts.description ?? ""
+        this.type = opts.type
+        this.defaultMemberPermissions = opts.defaultMemberPermissions ?? null
+        this.dmPermission = opts.dmPermission
+        this.guildIds = opts.guildIds
+
+        this.callback = opts.callback as (interaction: RunType[T]) => Promise<void>
+
+        if (opts.type === ApplicationCommandType.ChatInput) {
+            this.options = opts.options
+            
+            this.autoComplete = opts.autoComplete
+        }
+    }
+}
---
+++ src/lib/Listener.ts
@@ -0,0 +1,25 @@
+import type { ClientEvents } from "discord.js";
+
+interface ListnerOptions<E extends keyof ClientEvents> {
+    name: string;
+    event: keyof ClientEvents;
+    once?: boolean;
+
+    callback: (...args: ClientEvents[E]) => Promise<void>
+}
+
+export class Listener<E extends keyof ClientEvents = keyof ClientEvents> {
+    public name: string;
+    public event: keyof ClientEvents;
+    public once?: boolean;
+
+    public callback: (...args: ClientEvents[E]) => Promise<void>
+
+    public constructor(opts: ListnerOptions<E>) {
+        this.name = opts.name
+        this.event = opts.event
+        this.once = opts.once
+
+        this.callback = opts.callback
+    }
+}
